d11ab17382b43662e94b1cb0ab71d3a7bd8edc7a
Adam Nowak <adam.nowak@pwsz-ns.edu.pl>
2022-10-24 11:56:40 +0200

    v.1

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+
+
+
+int main()
+{
+	
+ int a,b,c;
+ cout<<"podaj a i b "<<endl;
+ cin>>a;
+ cin>>b;
+	
+if (a !=0)
+{
+	c=(-b/a);
+	cout<<"a="<<c<<endl;
+}
+else  if (a==0)
+
+{
+	cout<<"a nie mo¿ê byæ 0";
+}
+	return 0;
+}
+
+
+
