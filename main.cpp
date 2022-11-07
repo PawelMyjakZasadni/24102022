a0f97fbf98b1ad9667647a4836b9a78daf7cd8ce
Paulina <adam.nowak@pwsz-ns.edu.pl>
2022-11-07 10:52:50 +0100

    2.0

--- main.cpp
+++ main.cpp
@@ -1,15 +1,26 @@
 #include<iostream>
+#include<math.h>
 using namespace std;
-
-
+int a,b,c;
+int delta(int a, int b,int c)
+{return b*b-4*a*c;}
+float e(int a,int  b)
+{return ((-b)/(2*a));}
+float pierwX1(int  a, int  b, int  delta)
+{return (-b+sqrt(delta))/(2*a);}
+float pierwX2(int  a, int  b, int  delta)
+{return (-b-sqrt(delta))/(2*a);}
+int xliniowe(int c, int b)
+{return -c/b;}
 
 int main()
 {
 	
- int a,b,c,A,B,C,x,y,w,n;
+ float a,b,c,A,B,C,x,y,w,n,d;
  cout<<"podaj n do wybrnia co ma sie wykonac  "<<endl;
   cout<<"podaj n = 1 funkcja liniowa  "<<endl;
    cout<<"podaj n = 2 miejsce zerowe funkcji  "<<endl;
+   cout<<"podaj n = 3 funkcja kwadraatowa  "<<endl;
  cin>>n;
 if (n==1)
 	{
@@ -65,6 +76,40 @@ else if(n==2)
 			cout<<"rownanie ma jedno miejsce zerowe "<<x;
 	}
 }
+else if(n==3)
+{
+	cout<<"Rownanie kwadratowe:\n a*x*x+b*x+c=0 \n";
+cout<<"Podaj a: ";
+cin>>a;
+cout<<"Podaj b: ";
+cin>>b;
+cout<<"Podaj c: ";
+cin>>c;
+
+if (delta(a,b,c)<0)
+      {cout<<"Rownanie nie ma rozwiazan \n";}
+      else if (delta(a,b,c)==0)
+        {cout<<"Jest tylko jedno rozwiazanie:\n x="<<e(a,b);
+        cout<<"\n";}
+        else if (delta(a,b,c)>0)
+            {cout<<"Sa dwa rozwiazania rowne: \n"<<"x1="<<pierwX1(a,b,delta(a,b,c));
+            cout<<"\n"<<"x2="<<pierwX2(a,b,delta(a,b,c));
+            cout<<"\n";}
+if (a==0)
+   {
+          cout<<"X jest rowne:"<<xliniowe(c,b);
+          cout<<"\n";}
+   else
+   {
+       d=delta(a,b,c);
+       cout<<"Delta rownania wynosi:"<<d;
+       cout<<"\n";
+       
+}
+system ("pause");
+}
+ 
+
 
 	return 0;
 }
